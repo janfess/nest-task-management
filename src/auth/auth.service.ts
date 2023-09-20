0dc0e56887b49ec9093ee91a0b0fa0b6f2866964
Jan Esser <89969152+jan-esser-git@users.noreply.github.com>
2023-09-20 19:29:14 +0200

    Error Handling & Password hashing

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UsersRepository } from './users.repository';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(UsersRepository)
+    private usersRepository: UsersRepository,
+  ) {}
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return await this.usersRepository.createUser(authCredentialsDto);
+  }
+}
